diversenok/TokenUniverse

commit 9a8a074e1fb744a8024420163d7b3c1e35366acb

Make TNtxStatus store the last call information; prettify error messages
 - For open calls store access masks
 - For query/set calls store info classes

NtUtils/NtUtils.Exceptions.pas
@@ -3,42 +3,60 @@ unit NtUtils.Exceptions;
 interface
 
 uses
-  System.SysUtils, Ntapi.ntdef;
+  Winapi.WinNt, Ntapi.ntdef, System.SysUtils, System.TypInfo,
+  NtUtils.AccessMasks;
 
 const
   BUFFER_LIMIT = 1024 * 1024 * 256; // 256 MB
 
 type
+  TAccessMaskType = NtUtils.AccessMasks.TAccessMaskType;
+  TLastCallType = (lcOtherCall, lcOpenCall, lcQuerySetCall);
+
+  TLastCallInfo = record
+  case CallType: TLastCallType of
+    lcOpenCall:
+      (AccessMask: TAccessMask; AccessMaskType: TAccessMaskType);
+    lcQuerySetCall:
+      (InfoClass: Cardinal; InfoClassType: PTypeInfo);
+  end;
+
   /// <summary>
   ///  An enhanced NTSTATUS that stores the location of the failure.
   /// </summary>
   TNtxStatus = record
   private
+    FLocation: String;
     function GetWinError: Cardinal;
-    procedure SetWinError(Value: Cardinal);
+    procedure SetWinError(Value: Cardinal); inline;
     procedure FromLastWin32(RetValue: Boolean);
+    procedure SetLocation(Value: String); inline;
   public
     Status: NTSTATUS;
-    Location: String;
+    LastCall: TLastCallInfo;
+    function IsSuccess: Boolean; inline;
     property WinError: Cardinal read GetWinError write SetWinError;
-    function IsSuccess: Boolean;
+    property Win32Result: Boolean write FromLastWin32;
     procedure RaiseOnError;
     procedure ReportOnError;
-    function Matches(Status: NTSTATUS; Location: String): Boolean;
+  public
+    property Location: String read FLocation write SetLocation;
+    function Matches(Status: NTSTATUS; Location: String): Boolean; inline;
     function ToString: String;
     function MessageHint: String;
-    property Win32Result: Boolean write FromLastWin32;
   end;
 
 type
   ENtError = class(EOSError)
   public
     ErrorLocation: string;
+    LastCall: TLastCallInfo;
     function Matches(Location: String; Code: Cardinal): Boolean;
     class procedure Report(Status: Cardinal; Location: String);
     function ToWinErrorCode: Cardinal;
 
     constructor Create(Status: NTSTATUS; Location: String); reintroduce;
+    constructor CreateNtx(const Status: TNtxStatus);
     constructor CreateWin32(Win32Error: Cardinal; Location: String;
       Dummy: Integer = 0);
     constructor CreateLastWin32(Location: String);
@@ -78,7 +96,7 @@ begin
     Status := RtlxGetLastNtStatus;
 
     // Make sure that the code is not successful
-    if NT_SUCCESS(Status) then
+    if IsSuccess then
       Status := STATUS_UNSUCCESSFUL;
   end;
 end;
@@ -93,7 +111,7 @@ end;
 
 function TNtxStatus.IsSuccess: Boolean;
 begin
-  Result := NT_SUCCESS(Status);
+  Result := Integer(Status) >= 0; // inlined NT_SUCCESS from Ntapi.ntdef
 end;
 
 function TNtxStatus.Matches(Status: NTSTATUS; Location: String): Boolean;
@@ -108,14 +126,20 @@ end;
 
 procedure TNtxStatus.RaiseOnError;
 begin
-  if not NT_SUCCESS(Status) then
-    raise ENtError.Create(Status, Location);
+  if not IsSuccess then
+    raise ENtError.CreateNtx(Self);
 end;
 
 procedure TNtxStatus.ReportOnError;
 begin
-  if not NT_SUCCESS(Status) then
+  if not IsSuccess then
     ENtError.Report(Status, Location);
+end;
+
+procedure TNtxStatus.SetLocation(Value: String);
+begin
+  FLocation := Value;
+  LastCall.CallType := lcOtherCall;
 end;
 
 procedure TNtxStatus.SetWinError(Value: Cardinal);
@@ -140,6 +164,13 @@ end;
 constructor ENtError.CreateLastWin32(Location: String);
 begin
   Create(RtlxGetLastNtStatus, Location);
+end;
+
+constructor ENtError.CreateNtx(const Status: TNtxStatus);
+begin
+  ErrorLocation := Status.Location;
+  ErrorCode := Status.Status;
+  LastCall := Status.LastCall;
 end;
 
 constructor ENtError.CreateWin32(Win32Error: Cardinal; Location: String;

Core/TU.Suggestions.pas
@@ -23,14 +23,13 @@ implementation
 
 uses
   Ntapi.ntseapi, Winapi.WinError, Winapi.CommCtrl, Ntapi.ntdef, Ntapi.ntstatus,
-  NtUtils.Exceptions, NtUtils.ErrorMsg, NtUtils.Tokens.Misc;
+  NtUtils.Exceptions, NtUtils.ErrorMsg, Ntapi.ntpsapi, NtUtils.AccessMasks,
+  System.TypInfo;
 
 resourcestring
   TITLE_OS_ERROR = 'System error';
   TITLE_CONVERT = 'Conversion error';
   TITLE_BUG = 'This is definitely a bug...';
-  ERR_FMT = 'Last call: %s' + #$D#$A +
-   'Result: %s' + #$D#$A#$D#$A + '%s';
 
   BUGTRACKER = #$D#$A#$D#$A'If you known how to reproduce this error please ' +
     'help the project by opening an issue on our GitHub page'#$D#$A +
@@ -113,66 +112,85 @@ begin
     Exit(NEW_SAFER_IMP);
 end;
 
+function MatchesInfoClass(E: ENtError; InfoClass: TTokenInformationClass):
+  Boolean;
+begin
+  Result := (E.LastCall.CallType = lcQuerySetCall) and
+    (E.LastCall.InfoClass = Cardinal(InfoClass)) and
+    (E.LastCall.InfoClassType = TypeInfo(TTokenInformationClass));
+end;
+
+function MatchesInfoClassAndCode(E: ENtError; InfoClass: TTokenInformationClass;
+  ErrorCode: Cardinal): Boolean;
+begin
+  Result := (E.ErrorCode = ErrorCode) and MatchesInfoClass(E, InfoClass);
+end;
+
 function SuggestGetter(E: ENtError): String;
 begin
+  if E.ErrorLocation <> 'NtQueryInformationToken' then
+    Exit('');
+
   if E.ErrorCode = ERROR_ACCESS_DENIED then
   begin
-    if E.ErrorLocation = NtxFormatTokenQuery(TokenSource) then
-      Exit(GETTER_QUERY_SOURCE);
-
-    if E.ErrorLocation.StartsWith('GetTokenInformation:') then
+    if MatchesInfoClass(E, TokenSource) then
+      Exit(GETTER_QUERY_SOURCE)
+    else
       Exit(GETTER_QUERY);
   end;
 end;
 
 function SuggestSetter(E: ENtError): String;
 begin
+  if E.ErrorLocation <> 'NtSetInformationToken' then
+    Exit('');
+
   if E.ErrorCode = ERROR_ACCESS_DENIED then
   begin
-    if E.ErrorLocation = NtxFormatTokenSet(TokenSessionId) then
+    if MatchesInfoClass(E, TokenSessionId) then
       Exit(SETTER_SESSION);
 
-    if (E.ErrorLocation = NtxFormatTokenSet(TokenIntegrityLevel))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenUIAccess))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenMandatoryPolicy))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenOwner))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenPrimaryGroup))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenOrigin))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenVirtualizationAllowed))
-      or (E.ErrorLocation = NtxFormatTokenSet(TokenVirtualizationEnabled)) then
+    if MatchesInfoClass(E, TokenIntegrityLevel)
+      or MatchesInfoClass(E, TokenUIAccess)
+      or MatchesInfoClass(E, TokenMandatoryPolicy)
+      or MatchesInfoClass(E, TokenOwner)
+      or MatchesInfoClass(E, TokenPrimaryGroup)
+      or MatchesInfoClass(E, TokenOrigin)
+      or MatchesInfoClass(E, TokenVirtualizationAllowed)
+      or MatchesInfoClass(E, TokenVirtualizationEnabled) then
       Exit(SETTER_DEFAULT);
   end;
 
-  if E.Matches(NtxFormatTokenSet(TokenOwner), ERROR_INVALID_OWNER) then
+  if MatchesInfoClassAndCode(E, TokenOwner, ERROR_INVALID_OWNER) then
     Exit(SETTER_OWNER);
 
-  if E.Matches(NtxFormatTokenSet(TokenPrimaryGroup), ERROR_INVALID_PRIMARY_GROUP) then
+  if MatchesInfoClassAndCode(E, TokenPrimaryGroup, ERROR_INVALID_PRIMARY_GROUP) then
     Exit(SETTER_PRIMARY);
 
-  if E.Matches(NtxFormatTokenSet(TokenAuditPolicy), ERROR_INVALID_PARAMETER) then
+  if MatchesInfoClassAndCode(E, TokenAuditPolicy, ERROR_INVALID_PARAMETER) then
     Exit(SETTER_AUDIT_ONCE);
 
   if E.ErrorCode = ERROR_PRIVILEGE_NOT_HELD then
   begin
-    if E.ErrorLocation = NtxFormatTokenSet(TokenSessionId) then
+    if MatchesInfoClass(E, TokenSessionId) then
       Exit(SETTER_SESSION);
 
-    if E.ErrorLocation = NtxFormatTokenSet(TokenIntegrityLevel) then
+    if MatchesInfoClass(E, TokenIntegrityLevel) then
       Exit(SETTER_INTEGRITY_TCB);
 
-    if E.ErrorLocation = NtxFormatTokenSet(TokenUIAccess) then
+    if MatchesInfoClass(E, TokenUIAccess) then
       Exit(SETTER_UIACCESS_TCB);
 
-    if E.ErrorLocation = NtxFormatTokenSet(TokenMandatoryPolicy) then
+    if MatchesInfoClass(E, TokenMandatoryPolicy) then
       Exit(SETTER_POLICY_TCB);
 
-    if E.ErrorLocation = NtxFormatTokenSet(TokenVirtualizationAllowed) then
+    if MatchesInfoClass(E, TokenVirtualizationAllowed) then
       Exit(SETTOR_VIRT_PRIV);
 
-    if E.ErrorLocation = NtxFormatTokenSet(TokenOrigin) then
+    if MatchesInfoClass(E, TokenOrigin) then
       Exit(SETTER_ORIGIN_TCB);
 
-    if E.ErrorLocation = NtxFormatTokenSet(TokenAuditPolicy) then
+    if MatchesInfoClass(E, TokenAuditPolicy) then
       Exit(SETTER_AUDIT_PRIV);
   end;
 
@@ -197,7 +215,9 @@ begin
       Exit(SETTER_GROUPS_MODIFY);
   end;
 
-  if E.ErrorLocation = 'NtSetInformationProcess#ProcessAccessToken' then
+  if (E.ErrorLocation = 'NtSetInformationProcess') and
+    (E.LastCall.CallType = lcQuerySetCall) and
+    (E.LastCall.InfoClass = Cardinal(ProcessAccessToken)) then
   begin
    if E.ErrorCode = STATUS_NOT_SUPPORTED then
      Exit(ACTION_ASSIGN_NOT_SUPPORTED);
@@ -232,6 +252,8 @@ end;
 procedure ShowErrorSuggestions(ParentWnd: HWND; E: Exception);
 var
   Dlg: TASKDIALOGCONFIG;
+  ENt: ENtError;
+  Msg: String;
 begin
   FillChar(Dlg, SizeOf(Dlg), 0);
   Dlg.cbSize := SizeOf(Dlg);
@@ -253,18 +275,32 @@ begin
   end
   else if E is ENtError then
   begin
-    if not NT_ERROR(ENtError(E).ErrorCode) then
+    ENt := ENtError(E);
+
+    if not NT_ERROR(ENt.ErrorCode) then
       Dlg.pszMainIcon := TD_WARNING_ICON;
 
-    Dlg.pszMainInstruction := PWideChar(
-      StatusToDescription(ENtError(E).ErrorCode));
+    Dlg.pszMainInstruction := PWideChar(StatusToDescription(ENt.ErrorCode));
 
     if Dlg.pszMainInstruction = '' then
       Dlg.pszMainInstruction := PWideChar(TITLE_OS_ERROR);
 
-    Dlg.pszContent := PWideChar(Format(ERR_FMT, [ENtError(E).ErrorLocation,
-      StatusToString(ENtError(E).ErrorCode),
-      SysNativeErrorMessage(ENtError(E).ErrorCode)]) + SuggestAll(ENtError(E)));
+    Msg := 'Last call: ' + ENt.ErrorLocation;
+
+    case ENt.LastCall.CallType of
+      lcOpenCall:
+        Msg := Msg + #$D#$A + 'Desired access: ' + FormatAccess(
+          ENt.LastCall.AccessMask, ENt.LastCall.AccessMaskType);
+      lcQuerySetCall:
+        Msg := Msg + #$D#$A + 'Information class: ' + GetEnumName(
+          ENt.LastCall.InfoClassType, Integer(ENt.LastCall.InfoClass));
+    end;
+
+    Msg := Msg + #$D#$A + 'Result: ' + StatusToString(ENt.ErrorCode);
+    Msg := Msg + #$D#$A#$D#$A + SysNativeErrorMessage(ENt.ErrorCode);
+    Msg := Msg + SuggestAll(ENt);
+
+    Dlg.pszContent := PWideChar(Msg);
   end
   else
   begin

Core/TU.Tokens.pas
@@ -1458,8 +1458,8 @@ begin
   pAuditPolicy := Value.RawBuffer;
 
   try
-    NtxCheck(NtSetInformationToken(Token.hToken, TokenAuditPolicy,
-      pAuditPolicy, Value.RawBufferSize), NtxFormatTokenSet(TokenAuditPolicy));
+    NtxSetInformationToken(Token.hToken, TokenAuditPolicy, pAuditPolicy,
+      Value.RawBufferSize).RaiseOnError;
   finally
     Value.FreeRawBuffer(pAuditPolicy);
   end;

Headers/Ntapi.ntobapi.pas
@@ -9,9 +9,10 @@ uses
 
 type
   TObjectInformationClass = (
-    ObjectBasicInformation = 0, // q: TObjectBasicInformaion
-    ObjectNameInformation = 1, // q: UNICODE_STRING
-    ObjectTypeInformation = 2, // q: TObjectTypeInformation
+    ObjectBasicInformation = 0,     // q: TObjectBasicInformaion
+    ObjectNameInformation = 1,      // q: UNICODE_STRING
+    ObjectTypeInformation = 2,      // q: TObjectTypeInformation
+    ObjectTypesInformation = 3,     // q:
     ObjectHandleFlagInformation = 4 // q+s: TObjectHandleFlagInformation
   );
 

Headers/Ntapi.ntpsapi.pas
@@ -50,18 +50,80 @@ const
 
 type
   TProcessInfoClass = (
-    ProcessBasicInformation = 0, // q: TProcessBasinInformation
-    ProcessAccessToken = 9, // s: TProcessAccessToken
-    ProcessSessionInformation = 24, // q: Cardinal
-    ProcessWow64Information = 26, // q: NativeUInt
-    ProcessImageFileName = 27, // q: UNICODE_STRING
-    ProcessImageFileNameWin32 = 43 // q: UNICODE_STRING
+    ProcessBasicInformation = 0,       // q: TProcessBasinInformation
+    ProcessQuotaLimits = 1,
+    ProcessIoCounters = 2,
+    ProcessVmCounters = 3,
+    ProcessTimes = 4,
+    ProcessBasePriority = 5,
+    ProcessRaisePriority = 6,
+    ProcessDebugPort = 7,
+    ProcessExceptionPort = 8,
+    ProcessAccessToken = 9,            // s: TProcessAccessToken
+    ProcessLdtInformation = 10,
+    ProcessLdtSize = 11,
+    ProcessDefaultHardErrorMode = 12,
+    ProcessIoPortHandlers = 13,
+    ProcessPooledUsageAndLimits = 14,
+    ProcessWorkingSetWatch = 15,
+    ProcessUserModeIOPL = 16,
+    ProcessEnableAlignmentFaultFixup = 17,
+    ProcessPriorityClass = 18,
+    ProcessWx86Information = 19,
+    ProcessHandleCount = 20,
+    ProcessAffinityMask = 21,
+    ProcessPriorityBoost = 22,
+    ProcessDeviceMap = 23,
+    ProcessSessionInformation = 24,    // q: Cardinal
+    ProcessForegroundInformation = 25,
+    ProcessWow64Information = 26,      // q: NativeUInt
+    ProcessImageFileName = 27,         // q: UNICODE_STRING
+    ProcessLUIDDeviceMapsEnabled = 28,
+    ProcessBreakOnTermination = 29,
+    ProcessDebugObjectHandle = 30,
+    ProcessDebugFlags = 31,
+    ProcessHandleTracing = 32,
+    ProcessIoPriority = 33,
+    ProcessExecuteFlags = 34,
+    ProcessResourceManagement = 35,
+    ProcessCookie = 36,
+    ProcessImageInformation = 37,
+    ProcessCycleTime = 38,
+    ProcessPagePriority = 39,
+    ProcessInstrumentationCallback = 40,
+    ProcessThreadStackAllocation = 41,
+    ProcessWorkingSetWatchEx = 42,
+    ProcessImageFileNameWin32 = 43     // q: UNICODE_STRING
   );
 
   TThreadInfoClass = (
-    ThreadBasicInformation = 0, // q: TThreadBasicInformation
-    ThreadImpersonationToken = 5, // s: THandle
-    ThreadIsTerminated = 20 // q: LongBool
+    ThreadBasicInformation = 0,    // q: TThreadBasicInformation
+    ThreadTimes = 1,
+    ThreadPriority = 2,
+    ThreadBasePriority = 3,
+    ThreadAffinityMask = 4,
+    ThreadImpersonationToken = 5,  // s: THandle
+    ThreadDescriptorTableEntry = 6,
+    ThreadEnableAlignmentFaultFixup = 7,
+    ThreadEventPair = 8,
+    ThreadQuerySetWin32StartAddress = 9,
+    ThreadZeroTlsCell = 10,
+    ThreadPerformanceCount = 11,
+    ThreadAmILastThread = 12,
+    ThreadIdealProcessor = 13,
+    ThreadPriorityBoost = 14,
+    ThreadSetTlsArrayAddress = 15,
+    ThreadIsIoPending = 16,
+    ThreadHideFromDebugger = 17,
+    ThreadBreakOnTermination = 18,
+    ThreadSwitchLegacyState = 19,
+    ThreadIsTerminated = 20,       // q: LongBool
+    ThreadLastSystemCall = 21,
+    ThreadIoPriority = 22,
+    ThreadCycleTime = 23,
+    ThreadPagePriority = 24,
+    ThreadActualBasePriority = 25,
+    ThreadTebInformation = 26
   );
 
   // ProcessBasicInformation

Headers/Winapi.WinUser.pas
@@ -52,6 +52,7 @@ type
     LongBool; stdcall;
 
   TUserObjectInfoClass = (
+    UserObjectReserved = 0,
     UserObjectFlags = 1,    // q, s:
     UserObjectName = 2,     // q: PWideChar
     UserObjectType = 3,     // q: PWideChar

NtUtils/NtUtils.Lsa.pas
@@ -97,8 +97,7 @@ function LsaxEnumerateLogonSessions(out Luids: TLuidDynArray): TNtxStatus;
 implementation
 
 uses
-  Ntapi.ntdef, Ntapi.ntstatus, Winapi.NtSecApi, Ntapi.ntseapi, System.SysUtils,
-  NtUtils.AccessMasks;
+  Ntapi.ntdef, Ntapi.ntstatus, Winapi.NtSecApi, Ntapi.ntseapi, System.SysUtils;
 
 { Basic operation }
 
@@ -109,8 +108,11 @@ var
 begin
   InitializeObjectAttributes(ObjAttr);
 
-  Result.Location := 'LsaOpenPolicy for ' + FormatAccess(DesiredAccess,
-    objLsaPolicy);
+  Result.Location := 'LsaOpenPolicy';
+  Result.LastCall.CallType := lcOpenCall;
+  Result.LastCall.AccessMask := DesiredAccess;
+  Result.LastCall.AccessMaskType := TAccessMaskType.objLsaPolicy;
+
   Result.Status := LsaOpenPolicy(nil, ObjAttr, DesiredAccess, PolicyHandle);
 end;
 
@@ -130,8 +132,11 @@ begin
   if not Result.IsSuccess then
     Exit;
 
-  Result.Location := 'LsaOpenAccount for ' + FormatAccess(DesiredAccess,
-    objLsaAccount);
+  Result.Location := 'LsaOpenAccount';
+  Result.LastCall.CallType := lcOpenCall;
+  Result.LastCall.AccessMask := DesiredAccess;
+  Result.LastCall.AccessMaskType := TAccessMaskType.objLsaAccount;
+
   Result.Status := LsaOpenAccount(hPolicy, AccountSid, DesiredAccess,
     AccountHandle);
 

NtUtils/NtUtils.Objects.pas
@@ -165,7 +165,12 @@ var
   BufferSize: Cardinal;
 begin
   BufferSize := 0;
-  Result.Location := 'NtQueryObject [ObjectNameInformation]';
+
+  Result.Location := 'NtQueryObject';
+  Result.LastCall.CallType := lcQuerySetCall;
+  Result.LastCall.InfoClass := Cardinal(ObjectNameInformation);
+  Result.LastCall.InfoClassType := TypeInfo(TObjectInformationClass);
+
   Result.Status := NtQueryObject(hObject, ObjectNameInformation, nil, 0,
     @BufferSize);
 
@@ -187,7 +192,11 @@ end;
 function NtxQueryBasicInfoObject(hObject: THandle;
   out Info: TObjectBasicInformaion): TNtxStatus;
 begin
-  Result.Location := 'NtQueryObject [ObjectBasicInformation]';
+  Result.Location := 'NtQueryObject';
+  Result.LastCall.CallType := lcQuerySetCall;
+  Result.LastCall.InfoClass := Cardinal(ObjectBasicInformation);
+  Result.LastCall.InfoClassType := TypeInfo(TObjectInformationClass);
+
   Result.Status := NtQueryObject(hObject, ObjectBasicInformation, @Info,
     SizeOf(Info), nil);
 end;

commit 00c4e72e41ff9ec4142e63e71ff8996e51ddfd6c

Fix error reporting regression from commit 9a8a074

NtUtils/NtUtils.Exceptions.pas
@@ -168,8 +168,7 @@ end;
 
 constructor ENtError.CreateNtx(const Status: TNtxStatus);
 begin
-  ErrorLocation := Status.Location;
-  ErrorCode := Status.Status;
+  Create(Status.Status, Status.Location);
   LastCall := Status.LastCall;
 end;
 